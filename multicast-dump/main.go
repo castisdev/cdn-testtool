8ae05a1609793d8233851e1ab04254ac34ed665a
pinkcubrick <pinkcubrick@gmail.com>
2019-06-28 11:19:16 +0900

    multicast-dump: ip binding 처리
    기존: 224.0.0.1:5000 수신시 224.0.0.2:5000 도 수신했었음

--- multicast-dump/main.go
+++ multicast-dump/main.go
@@ -2,60 +2,64 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
 	"net"
 	"os"
 	"strings"
-	"sync"
 	"time"
+
+	"golang.org/x/net/ipv4"
 )
 
 func main() {
 	duration := flag.Duration("duration", 1*time.Minute, "dump duration")
 	flag.Parse()
 
-	var wg sync.WaitGroup
-	{
-		wg.Add(1)
-		go dump(flag.Arg(0), *duration, &wg)
-	}
-
-	wg.Wait()
-}
-
-func dump(addrstr string, duration time.Duration, wg *sync.WaitGroup) {
-	defer wg.Done()
+	addrstr := flag.Arg(0)
 
 	filename := strings.Replace(addrstr, ".", "_", -1)
 	filename = strings.Replace(filename, ":", "_", -1)
 	dmp, err := os.Create(filename + ".ts")
 	if err != nil {
-		log.Fatal()
+		log.Fatalf("failed to create file, %v", err)
 	}
 	defer dmp.Close()
 
-	addr, err := net.ResolveUDPAddr("udp", addrstr)
+	ipv4addr, err := net.ResolveUDPAddr("udp4", addrstr)
 	if err != nil {
-		log.Fatal()
+		log.Fatalf("failed to resolve udp address, %v", err)
 	}
 
-	conn, err := net.ListenMulticastUDP("udp", nil, addr)
+	conn, err := net.ListenUDP("udp4", ipv4addr)
 	if err != nil {
-		log.Fatal()
+		fmt.Printf("ListenUDP error %v\n", err)
+		return
 	}
 	defer conn.Close()
 
-	log.Print("start to dump, ", addrstr)
-	buf := make([]byte, 1316+12)
-	for start := time.Now(); time.Now().Sub(start) <= duration; {
-		n, _, err := conn.ReadFromUDP(buf)
-		if err != nil {
-			log.Fatal()
-		}
+	pc := ipv4.NewPacketConn(conn)
 
+	ifs, err := net.Interfaces()
+	if err != nil {
+		log.Fatalf("failed to get interfaces, %v", err)
+	}
+	for _, ifi := range ifs {
+		if err := pc.JoinGroup(&ifi, ipv4addr); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	log.Printf("start to dump, %v, %v", addrstr, *duration)
+	buf := make([]byte, 1316+12)
+	for start := time.Now(); time.Now().Sub(start) <= *duration; {
+		n, _, err := conn.ReadFrom(buf)
+		if err != nil {
+			log.Fatalf("failed to read, %v", err)
+		}
 		_, err = dmp.Write(buf[12:n])
 		if err != nil {
-			log.Fatal()
+			log.Fatalf("failed to write to file, %v", err)
 		}
 	}
 	log.Print("end to dump, ", addrstr)
